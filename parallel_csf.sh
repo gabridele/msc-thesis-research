cd6a8add5e1f539be0eb30b3a5ed4863d974dd08
gabridele <125871960+gabridele@users.noreply.github.com>
2024-05-06 20:50:13 +0200

    update

--- parallel_csf.sh
+++ parallel_csf.sh
@@ -16,36 +16,30 @@ fi
 
 #0 make timings
 
-function timings {
- for subj in `cat "subject_id_with_exclusions.txt"`; do
-  derivatives_dir="derivatives/$subj/func"
-  cd "$subj/func"
+for subj in "$input"; do
+	derivatives_dir="derivatives/$subj/func"
+	cd "$subj/func"
 
-  echo -e "Processing subject: $subj...\n"
+	echo -e "Processing subject: $subj...\n"
 
-  cat ${subj}_task-scap_events.tsv | awk '{if ($3 = 1 && $3 = 4) {print $1, 6.5, 1}}' > "../../$derivatives_dir/${subj}_task-scap_low_WM_1500.txt"
-  cat ${subj}_task-scap_events.tsv | awk '{if ($3 = 2 && $3 = 5) {print $1, 8.0, 1}}' > "../../$derivatives_dir/${subj}_task-scap_low_WM_3000.txt"
-  cat ${subj}_task-scap_events.tsv | awk '{if ($3 = 3 && $3 = 6) {print $1, 9.5, 1}}' > "../../$derivatives_dir/${subj}_task-scap_low_WM_4500.txt"	
-  cat ${subj}_task-scap_events.tsv | awk '{if ($3 = 7 && $3 = 10) {print $1, 6.5, 1}}' > "../../$derivatives_dir/${subj}_task-scap_high_WM_1500.txt"
-  cat ${subj}_task-scap_events.tsv | awk '{if ($3 = 8 && $3 = 11) {print $1, 8.0, 1}}' > "../../$derivatives_dir/${subj}_task-scap_high_WM_3000.txt"
-  cat ${subj}_task-scap_events.tsv | awk '{if ($3 = 9 && $3 = 12) {print $1, 9.5, 1}}' > "../../$derivatives_dir/${subj}_task-scap_high_WM_4500.txt"
+	cat ${subj}_task-scap_events.tsv | awk '{if ($3 == 1 && $3 == 4) {print $1, 6.5, 1}}' > "../../$derivatives_dir/${subj}_task-scap_low_WM_1500.txt" &
+	cat ${subj}_task-scap_events.tsv | awk '{if ($3 == 2 && $3 == 5) {print $1, 8.0, 1}}' > "../../$derivatives_dir/${subj}_task-scap_low_WM_3000.txt" &
+	cat ${subj}_task-scap_events.tsv | awk '{if ($3 == 3 && $3 == 6) {print $1, 9.5, 1}}' > "../../$derivatives_dir/${subj}_task-scap_low_WM_4500.txt" &
+	cat ${subj}_task-scap_events.tsv | awk '{if ($3 == 7 && $3 == 10) {print $1, 6.5, 1}}' > "../../$derivatives_dir/${subj}_task-scap_high_WM_1500.txt" &
+	cat ${subj}_task-scap_events.tsv | awk '{if ($3 == 8 && $3 == 11) {print $1, 8.0, 1}}' > "../../$derivatives_dir/${subj}_task-scap_high_WM_3000.txt" &
+	cat ${subj}_task-scap_events.tsv | awk '{if ($3 == 9 && $3 == 12) {print $1, 9.5, 1}}' > "../../$derivatives_dir/${subj}_task-scap_high_WM_4500.txt" &
 
-  # Convert to AFNI format
-  echo "Converting to AFNI format..."
-  timing_tool.py -fsl_timing_files "../../$derivatives_dir/${subj}_task-scap_low_WM_1500.txt" -write_timing "../../$derivatives_dir/${subj}_task-scap_low_WM_1500.1D"
-  timing_tool.py -fsl_timing_files "../../$derivatives_dir/${subj}_task-scap_low_WM_3000.txt" -write_timing "../../$derivatives_dir/${subj}_task-scap_low_WM_3000.1D"
-  timing_tool.py -fsl_timing_files "../../$derivatives_dir/${subj}_task-scap_low_WM_4500.txt" -write_timing "../../$derivatives_dir/${subj}_task-scap_low_WM_4500.1D"	
-  timing_tool.py -fsl_timing_files "../../$derivatives_dir/${subj}_task-scap_high_WM_1500.txt" -write_timing "../../$derivatives_dir/${subj}_task-scap_high_WM_1500.1D"
-  timing_tool.py -fsl_timing_files "../../$derivatives_dir/${subj}_task-scap_high_WM_3000.txt" -write_timing "../../$derivatives_dir/${subj}_task-scap_high_WM_3000.1D"
-  timing_tool.py -fsl_timing_files "../../$derivatives_dir/${subj}_task-scap_high_WM_4500.txt" -write_timing "../../$derivatives_dir/${subj}_task-scap_high_WM_4500.1D"
+	# Convert to AFNI format
+	echo "Converting to AFNI format..."
+	timing_tool.py -fsl_timing_files "../../$derivatives_dir/${subj}_task-scap_low_WM_1500.txt" -write_timing "../../$derivatives_dir/${subj}_task-scap_low_WM_1500.1D" &
+	timing_tool.py -fsl_timing_files "../../$derivatives_dir/${subj}_task-scap_low_WM_3000.txt" -write_timing "../../$derivatives_dir/${subj}_task-scap_low_WM_3000.1D" &
+	timing_tool.py -fsl_timing_files "../../$derivatives_dir/${subj}_task-scap_low_WM_4500.txt" -write_timing "../../$derivatives_dir/${subj}_task-scap_low_WM_4500.1D"	&
+	timing_tool.py -fsl_timing_files "../../$derivatives_dir/${subj}_task-scap_high_WM_1500.txt" -write_timing "../../$derivatives_dir/${subj}_task-scap_high_WM_1500.1D" &
+	timing_tool.py -fsl_timing_files "../../$derivatives_dir/${subj}_task-scap_high_WM_3000.txt" -write_timing "../../$derivatives_dir/${subj}_task-scap_high_WM_3000.1D" &
+	timing_tool.py -fsl_timing_files "../../$derivatives_dir/${subj}_task-scap_high_WM_4500.txt" -write_timing "../../$derivatives_dir/${subj}_task-scap_high_WM_4500.1D" &
 
-  cd ../..
- done
-}
-
-export -f timings
-
-parallel -j "$numjobs" timings {}
+	cd ../..
+done
 
 echo "###################################################################" 
 echo ".....................Smoothing EPI....................."
