bb67a5d73ec41fa6e73b6605a7dec1613dd3a880
gabridele <125871960+gabridele@users.noreply.github.com>
2024-05-28 15:18:56 +0200

    Update vols_concat.sh

--- vols_concat.sh
+++ vols_concat.sh
@@ -1,48 +1,51 @@
-cd derivatives/
-
-function vols_concat {
+function header {
  input="$1"
- sub_id=$(echo "$input" | sed 's/^.*\/sub/sub/')
- sub_id="${sub_id%%/dwi*}"
+ file_name=$(basename "$input")
+ #sub_id=$(echo "$input" | sed 's/^.*\/sub/sub/')
+ #sub_id="${sub_id%%/dwi*}"
  output="./concat_vols.tsv"
+ 
+ for subj in $(cat "subject_id_with_exclusions.txt"); do
+  echo "Processing subject: $subj"
+  echo "File name: $file_name"
 
- echo -e "$sub_id\n$(cat "$input")" > "${input}.with_header"
- # if output file doesn't exist, create it with header and content
- if [ ! -f "$output" ]; then
-   mv "${input}.with_header" "$output"
- else
-   # else append content of input file as new column
-   paste "$output" "${input}.with_header" > "${output}.tmp" && mv "${output}.tmp" "$output"
- fi
- rm "${input}.with_header"
+  cd derivatives/$subj/dwi/Schaefer2018_400Parcels_Tian_Subcortex_S4_1mm_by_parcel/
+
+  echo -e "$subj\n$(cat "$file_name")" > "${file_name%.txt}_"$subj"_with_header.txt"
+  cd ../../../..
+ done
 
 }
 
-export -f vols_concat
+export -f header
 find "." -type f -name 'AllVols.txt' > "./input_files.txt"
-cat "./input_files.txt" | parallel -j 1 vols_concat {}
+cat "./input_files.txt" | parallel -j 1 header {}
 rm "./input_files.txt"
 cd ..
 
 
 
+function vols_concat {
+  output_file="./concat_vols.tsv"
+  temp_dir=$(mktemp -d)
 
+  for subj in $(cat "subject_id_with_exclusions.txt"); do
+    echo "Processing subject: $subj"
+    input_file="derivatives/$subj/dwi/Schaefer2018_400Parcels_Tian_Subcortex_S4_1mm_by_parcel/AllVols_${subj}_with_header.txt"
+    if [[ -f "$input_file" ]]; then
+      cp "$input_file" "$temp_dir/${subj}_vols.txt"
+    else
+      echo "File not found: $input_file"
+    fi
+  done
 
+  # Use paste to concatenate all files column-wise
+  paste "$temp_dir"/* > "$output_file"
 
-
-
-
-
-
-
-
-
-
-
-
+  # Clean up temporary directory
+  rm -r "$temp_dir"
+}
 
 export -f vols_concat
 
-find "." -type f -name 'AllVols.txt' > "./input_files.txt"
-cat "./input_files.txt" | parallel -j 1 vols_concat {}
-rm "./input_files.txt"
+vols_concat
