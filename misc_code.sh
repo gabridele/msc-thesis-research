f19b17cfc4e777fa66abdc8bb0c85fc00b83b02e
gabridele <125871960+gabridele@users.noreply.github.com>
2024-05-28 11:02:03 +0200

    update

--- misc_code.sh
+++ misc_code.sh
@@ -5,13 +5,32 @@
 fslstats $atlas_name -l 0 -R
 
 #2: run this by modiying N
-#N=454
+mkdir rois
+N=454
 for ii in {1..454}
 do
-       fslmaths $atlas_name -thr $ii -uthr $ii -bin roi${ii}.nii.gz
-       fslstats roi${ii}.nii.gz -V > roi_${ii}.txt
+       fslmaths $atlas_name -thr $ii -uthr $ii -bin rois/roi_${ii}.nii.gz
+       fslstats rois/roi_${ii}.nii.gz -V > rois/roi_${ii}.txt
 done
 
+# combine rois, only takes first column
+
+cd rois
+output_file="combined.txt"
+> $output_file
+
+for i in {1..454}
+do
+ input_file="roi_$i.txt"
+ first_column=$(awk '{print $1}' $input_file)
+ echo $first_column >> $output_file
+done
+
+#remove roi_${ii}.*
+for ii in {1..454}
+do
+       rm roi_${ii}.*
+done
 
 #concatenate registered T1s and view them as time series
 
