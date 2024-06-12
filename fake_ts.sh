4601031acad608453a7085071d45bd1b4505fecc
gabridele <125871960+gabridele@users.noreply.github.com>
2024-06-12 16:52:14 +0200

    Create fake_ts.sh

---
+++ fake_ts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+path_der="derivatives/"
+
+function fake_ts {
+  input="$1"
+  output="${input%.nii.gz}_2vol_ts.nii.gz"
+
+  fslmerge -t "$output" "$input" "$input"
+
+  echo -e "Done with $output"
+}
+
+export -f fake_ts
+
+find "$path_der" -type f -name '*_diff_wm.nii.gz' > "$path_der/fake_ts_files.txt"
+
+N=20
+(
+for ii in $(cat "$path_der/fake_ts_files.txt"); do 
+   ((i=i%N)); ((i++==0)) && wait
+   fake_ts "$ii" & 
+done
+)
+rm "$path_der/fake_ts_files.txt"
