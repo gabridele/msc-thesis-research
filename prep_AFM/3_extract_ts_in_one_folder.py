391d88f367980f6e04bfe798d8dff2ee1f9c5e6c
gabridele <125871960+gabridele@users.noreply.github.com>
2024-06-23 12:06:55 +0200

    update

--- prep_AFM/3_extract_ts_in_one_folder.py
+++ prep_AFM/3_extract_ts_in_one_folder.py
@@ -10,10 +10,10 @@ import scipy.io as sio
 def extract_ts(subject_name):
 
    ts,labels=regions.img_to_signals_labels(image.load_img(subject_name,dtype='float64'), image.load_img(path_to_atlas), mask_img=None, background_label=0)
-
+   
    sub_name_no_ending=('.').join(subject_name.split('/')[-1].split('.')[:-2])
    
-   np.save(out_dir+'/'+sub_name_no_ending,ts)
+   np.save(out_dir+'/'+sub_name_no_ending,'_1vol_',ts)
 
 
 def main():
@@ -22,15 +22,23 @@ def main():
    global path_to_atlas
 
    path_to_atlas = os.getcwd()+'/derivatives/templates/'+'Schaefer2018_400Parcels_Tian_Subcortex_S4_1mm_2009c_NLinAsymm.nii.gz'
+   
    print(path_to_atlas)
+   
    global out_dir
+   
    out_dir = os.getcwd()+'/derivatives/'
    print(out_dir)
+   
    #path nella cartella coi dati func
    single_files = os.getcwd()+'/derivatives/sub*/func/*_2vol_ts.nii.gz'
+   
    print(single_files)
+   
    subjects = sorted(glob.glob(single_files))
+   
    print(subjects)
+   
    pool = Pool(processes=2)
 
    pool.map(extract_ts, subjects)
