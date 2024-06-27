84bc0146c2cc3fa68df58cb10f04a3eaa55d5981
gabridele <125871960+gabridele@users.noreply.github.com>
2024-06-27 09:41:26 +0200

    Update rsfmri_preprocess_07_compute_fd.sh

--- ludovicos_regression_scripts(backup)/rsfmri_preprocess_07_compute_fd.sh
+++ ludovicos_regression_scripts(backup)/rsfmri_preprocess_07_compute_fd.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-# This script computes the framewise displacement (FD) that will be used for censoring. 
-# -----------------------------------------------------------
-# Script written by Ludovico Coletta
-# Nilab, FBK (2022)
-# -----------------------------------------------------------
+
+path_der="derivatives/"
 
 function compute_fd {
 
     ts=$1
-    subject=$(basename $ts _slt.nii.gz)
+    sub_id=$(basename "$ts" | grep -oP 'sub-\d+')
     sub_folder=$(dirname $ts)
+    metric_output="${ts%_preproc.nii.gz}_fd.txt"
+    output="${ts%_preproc.nii.gz}_confound"
     
     fsl_motion_outliers \
       -i $ts \
       --fd \
-      -s ${sub_folder}/${subject}_fd.txt \
-      -o ${sub_folder}/${subject}_confound \
+      -s $metric_output \
+      -o $output \
       -v \
-      &> ${sub_folder}/log_${subject}_fd_computation.txt
+      &> ${sub_folder}/log_${sub_id}_fd_computation.txt
 
 }
+
 export -f compute_fd
 
-# main code starts here
+find "$path_der" -type f -name '*_task-rest_bold_space-MNI*_preproc.nii.gz' > "$path_der/rsfmri_files.txt"
 
-study_folder=IntraOpMap_RestingState #EDIT HERE
-numjobs=16 #EDIT HERE
-
-echo $PWD/${study_folder}/derivatives/CustomPrepro/Pre/sub-*/func/*_slt.nii.gz | tr " " "\n" > subject_list.txt #EDIT HERE
-
-parallel \
-    -j $numjobs \
-    compute_fd {} \
-    < subject_list.txt
-
+N=40
+(
+for ii in $(cat "$path_der/rsfmri_files.txt"); do 
+   ((i=i%N)); ((i++==0)) && wait
+   compute_fd "$ii" &
+done
+)
+rm "$path_der/rsfmri_files.txt"
 
