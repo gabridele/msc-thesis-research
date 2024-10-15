bcea7b3b5f632c1d5ac935426134328d52e06c8e
gabridele <gabrieledele@gmail.com>
2024-10-15 14:17:23 +0200

    update

--- _prep_AFM/2.1_extract_FC_rsfmri.py
+++ _prep_AFM/2.1_extract_FC_rsfmri.py
@@ -1,9 +1,36 @@
+# script to compute correlation between brain regions from rsfmri data. Aka getting the FC matrix
+
 from nilearn import image, regions
 import numpy as np
 import os
 from scipy import stats
 import glob
 from multiprocessing import Pool
+
+# def extract_ts(subject_name):
+
+#     sub_id = subject_name.split('/')[-3]
+
+#     ts, labels = regions.img_to_signals_labels(
+#         image.load_img(subject_name, dtype='float64'),
+#         image.load_img(path_to_atlas),
+#         mask_img=None,
+#         background_label=0
+#     )
+
+#     ts = ts.T
+#     ts[ts == 0] = np.nan
+#     # get FC matrix via Pearson correlation
+#     correlation_result = stats.pearsonr(ts)
+    
+#     # extract correlation matrix
+#     correlation_matrix = correlation_result.correlation
+
+#     # set diagonal to 0
+#     np.fill_diagonal(correlation_matrix, 0)
+#     correlation_matrix[correlation_matrix == np.nan] = 0
+#     output_path = os.path.join(out_dir, sub_id, 'func', sub_id + '_FC_correlation_matrix.npy')
+#     np.save(output_path, correlation_matrix)
 
 def extract_ts(subject_name):
 
@@ -17,19 +44,16 @@ def extract_ts(subject_name):
     )
 
     ts = ts.T
-    ts[ts == 0] = np.nan
-    # get FC matrix via Pearson correlation
-    correlation_result = stats.pearsonr(ts)
     
-    # extract correlation matrix
-    correlation_matrix = correlation_result.correlation
+    # get FC matrix via Pearson correlation
+    correlation_matrix = np.corrcoef(ts)
 
-    # set diagonal to 0
+    # set the diagonal to 0
     np.fill_diagonal(correlation_matrix, 0)
-    correlation_matrix[correlation_matrix == np.nan] = 0
+
     output_path = os.path.join(out_dir, sub_id, 'func', sub_id + '_FC_correlation_matrix.npy')
     np.save(output_path, correlation_matrix)
-
+    
 def main():
     
     global path_to_atlas
