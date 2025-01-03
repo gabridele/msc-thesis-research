9dd978db07f94338646c787fb4d03de2adc11696
gabridele <gabrieledele@gmail.com>
2025-01-03 17:15:28 +0100

    fix wrong syntax

--- stats/atlas_label_vector.py
+++ stats/atlas_label_vector.py
@@ -6,13 +6,13 @@ def create_label_vector(file_path):
 
     label_vector = []
     for i in range(0, len(lines), 2):
-        label = lines[i].strip()
-        if 'rh' or 'RH' in label:
-            label_vector.append(True)
-        elif 'lh' or 'LH' in label:
-            label_vector.append(False)
+        label = lines[i].strip().lower()  # Convert to lowercase for consistency
+        if 'rh' in label:
+            label_vector.append(True)  # Right hemisphere
+        elif 'lh' in label:
+            label_vector.append(False)  # Left hemisphere
 
-    return label_vector
+    return np.array(label_vector)
 
 # Example usage
 file_path = '/Users/gabrieledele/Downloads/sSchaefer2018_400Parcels_7Networks_order_Tian_Subcortex_S4_label.txt'
